35476a7123aa651777af7de2484034b3e19deb78
Kashbel <1kashbel+optcutils@gmail.com>
2017-11-13 13:09:06 +0100

    Fixed gulp file and added stamina changelog

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const minify = require('gulp-minify');
+
+const scripts = [
+    {
+        path: './src/stamina/stamina.js',
+        name: 'stamina.js',
+        destination: './stamina/assets/js/'
+    }
+];
+
+gulp.task('scripts', function(){
+    return scripts.map(function(file){
+        browserify(file.path)
+        .transform('babelify', {presets: ['env']})
+        .bundle()
+        .pipe(source(file.name))
+        .pipe(buffer())
+        .pipe(minify({ext:{min:'.js'}, noSource: true}))
+        .pipe(gulp.dest(file.destination));
+});});
