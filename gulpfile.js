262b0b3ccadadbe3ee4179c4ee63b4f6a1a4fcad
Kashbel <1kashbel@gmail.com>
2017-11-15 18:48:23 +0100

    [BookHelper] Initial release of BookHelper working

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@ const browserify = require('browserify');
 const source = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
 const minify = require('gulp-minify');
-const push = require('gulp-git-push');
 
 const scripts = [
     {
@@ -17,18 +16,6 @@ const scripts = [
         destination: './bookhelper/assets/js/'
     }
 ];
-
-gulp.task('push', function(){
-    return scripts.map(function(file){
-        browserify(file.path)
-        .transform('babelify', {presets: ['env']})
-        .bundle()
-        .pipe(source(file.name))
-        .pipe(buffer())
-        .pipe(minify({ext:{min:'.js'}, noSource: true}))
-        .pipe(gulp.dest(file.destination))
-        .pipe(push());
-});});
 
 gulp.task('scripts', function(){
     return scripts.map(function(file){
